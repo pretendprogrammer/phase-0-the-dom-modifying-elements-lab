2e4f613ad3f71c15317bc9250938ebf7ce93d036
Yosef <shmuli5770@gmail.com>
2021-03-13 18:02:26 -0500

    Created .js file with passing edits to DOM

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById('main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Yosef is the champion';
+document.appendChild(newHeader);
